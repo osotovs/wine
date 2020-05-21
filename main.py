bab547b4334ff88d9ef74c50c20d97bfe9426739
sind <sergeyosotov33@gmail.com>
2020-05-21 21:39:01 +0700

    '8/19'

--- main.py
+++ main.py
@@ -10,9 +10,7 @@ env = Environment(
 template = env.get_template('template.html')
 
 date_born =  dt.date(year=1920, month=1,day=1)
-print(date_born)
 date_now = dt.date.today()
-print(date_now.year)
 date_age = date_now.year - date_born.year
 
 rendered_page = template.render(
